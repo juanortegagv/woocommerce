c22763f80059a5247ad1dd86a9ab4235835aa79a
2MStyle <jorteganlpp@gmail.com>
2018-01-11 07:47:33 -0400

    Iva

--- ecommerce/wp-content/plugins/IVA/IVA.php
+++ ecommerce/wp-content/plugins/IVA/IVA.php
@@ -1,4 +1,4 @@
-/*
+<?php /*
 Plugin Name: IVA Venezuela.
 Plugin URI: https://github.com/juanortegagv/woocommerce/
 Description: Genera el IVA de 7% y 9% dependiendo del precio
@@ -8,3 +8,15 @@ Author URI: https://github.com/juanortegagv/woocommerce
 
 Copyright: 2018- Juan Ortega
 */
+
+add_filter("","iva_porcentaje");
+
+function iva_porcentaje($monto){
+	$total = 0;
+
+	if($monto>2000000){
+		$total = ($monto * 0.07)+$monto;
+	}else{
+		$total = ($monto * 0.09)+$monto;
+	}	
+}
